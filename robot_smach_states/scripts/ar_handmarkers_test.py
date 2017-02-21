f34082dae8624214b4add674bb35072f362f911e
Kwin van der Veen <kwinvdv@gmail.com>
2017-02-21 20:14:48 +0100

    fixed amigo instance

--- robot_smach_states/scripts/ar_handmarkers_test.py
+++ robot_smach_states/scripts/ar_handmarkers_test.py
@@ -8,19 +8,25 @@ In short, amigo first brings its hands in a grasping position, then
 looks at one of the hands and publishes the tf from that hand to the
 marker on that hand."""
 
-import robot_skills.amigo
+from robot_skills.amigo import Amigo
 import rospy
 
 if __name__ == "__main__":
     rospy.init_node("hands_ARmarkers_test", anonymous=True)
+    amigo = Amigo()
 
     # Create topics for the left and right hand
     # left_tf  = rospy.Publisher('lefthand_error',  std_msgs.msg.String, queue_size=10)
     # right_tf = rospy.Publisher('righthand_error', std_msgs.msg.String, queue_size=10)
 
     # Position the arms in grasping position and look at left hand
+    rospy.loginfo('Sending left arm goal')
     amigo.leftArm._send_joint_trajectory([[0,0.5,0.3,1.2,-0.15,-0.1,-0.2]])
+
+    rospy.loginfo('Sending right arm goal')
     amigo.rightArm._send_joint_trajectory([[0,0.5,0.3,1.2,-0.15,-0.1,-0.2]])
+
+    rospy.loginfo('Sending head goal')
     amigo.head.look_at_hand("left")
 
     # Publish tf
