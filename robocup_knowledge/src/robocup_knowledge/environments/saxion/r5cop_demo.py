4477f869e126030f997b78931b8691d29291a3b1
Rein Appeldoorn <reinzor@gmail.com>
2017-03-01 15:39:31 +0100

    Added saxion knowledge

---
+++ robocup_knowledge/src/robocup_knowledge/environments/saxion/r5cop_demo.py
@@ -0,0 +1,26 @@
+starting_point = "initial_pose"
+
+# required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
+inspection_places = [
+    {"entity_id": "dinner_table",
+     "room_id": "livingroom",
+     "navigate_area": "near",
+     "segment_areas": ["under"]},
+
+]
+
+known_types = [
+    "coke",
+    "fanta",
+    "mentos",
+    "bifrutas",
+    "beer",
+    "coffee_pads",
+    "deodorant",
+    "ice_tea",
+    "sprite",
+    "tea",
+    "water",
+    "xylit24_spearmint",
+    "xylit24_white"]
+
